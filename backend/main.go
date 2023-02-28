99b062c312623d78cba36ff7ee0db5fdf5aa3380
Sakyuba Noe <398noe@gmail.com>
2023-02-28 17:09:44 +0900

    Make init gin instance

--- backend/main.go
+++ backend/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-gonic/gin"
 	"github.com/haisin-official/haisin/config"
 )
 
 func main() {
+	r := gin.Default()
+
+	port := config.GetEnv("HTTP_PORT")
+	r.Run(":" + port)
+
 	fmt.Println("hais.in version:", config.GetEnv("VERSION"))
+	fmt.Println("Listening:", port)
+
 }
