3f880f4f31e9aab542f9f6bc3f6fdbe9e1528e6b
Sakyuba Noe <398noe@gmail.com>
2023-03-07 04:24:02 +0900

    Add router for api works

--- backend/main.go
+++ backend/main.go
@@ -5,15 +5,16 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/haisin-official/haisin/config"
+	"github.com/haisin-official/haisin/routes"
 )
 
 func main() {
 	r := gin.Default()
+	routes.Router(r)
 
 	port := config.GetEnv("HTTP_PORT")
 	r.Run(":" + port)
 
 	fmt.Println("hais.in version:", config.GetEnv("VERSION"))
 	fmt.Println("Listening:", port)
-
 }
