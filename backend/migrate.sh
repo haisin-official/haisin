fbaac37d91ad389129429b1d7402859daa760c3b
Sakyuba Noe <398noe@gmail.com>
2023-03-07 02:39:11 +0900

    Fix read source

--- backend/migrate.sh
+++ backend/migrate.sh
@@ -3,12 +3,14 @@
 # Generate migration file
 go generate ./ent
 
+# description by ent
+# go run -mod=mod entgo.io/ent/cmd/ent describe ./ent/schema
 atlas migrate diff init \
   --dir "file://ent/migrate/migrations" \
   --to "ent://ent/schema" \
   --dev-url "docker://postgres/14/haisin?search_path=public"
 
-source ./.env
+. ./.env
 atlas migrate apply \
   --dir "file://ent/migrate/migrations" \
   --url ${DB_URL}
