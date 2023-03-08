f02a59094cb311bbeecc58c0ec53c1ebb14cd2c1
Sakyuba Noe <398noe@gmail.com>
2023-03-08 19:49:58 +0900

    Make simple vars

--- backend/app/http/responses/OAuth/CallbackResponse.go
+++ backend/app/http/responses/OAuth/CallbackResponse.go
@@ -7,8 +7,8 @@ type OAuthCallback struct {
 }
 
 type OAuthCallbackUser struct {
-	Uuid      uuid.UUID `json:"uuid" binding:"required"`
-	UserEmail string    `json:"email" binding:"required"`
-	Slug      string    `json:"slug" binding:"required"`
-	Ga        *string   `json:"ga"`
+	Uuid  uuid.UUID `json:"uuid" binding:"required"`
+	Email string    `json:"email" binding:"required"`
+	Slug  string    `json:"slug" binding:"required"`
+	Ga    *string   `json:"ga"`
 }
