76f205598b860bb2192052ed8a2a1e3a39fa36ca
Sakyuba Noe <398noe@gmail.com>
2023-03-10 01:22:53 +0900

    Add new session manager

---
+++ backend/config/session/session.go
@@ -0,0 +1,83 @@
+package session
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/haisin-official/haisin/app/utils"
+	"github.com/haisin-official/haisin/config"
+	goRedis "github.com/redis/go-redis/v9"
+)
+
+type Manager struct {
+	MaxAge   int
+	Path     string
+	Domain   string
+	Secure   bool
+	HttpOnly bool
+}
+
+// Cookieが持つ値
+type Store struct {
+	SessionId string `json:"session_id" binding:"required"`
+	UserId    string `json:"user_id" binding:"required"`
+}
+
+var (
+	r       *goRedis.Client
+	manager Manager
+)
+
+func init() {
+	url := config.GetEnv("REDIS_URL")
+	opt, err := goRedis.ParseURL(url)
+	if err != nil {
+		log.Panicf("Fatal Error of redis 🚫\n %v", err)
+	}
+	r = goRedis.NewClient(opt)
+
+	fmt.Println("Success connection to redis ✅")
+
+	manager.MaxAge = 86400
+	manager.Path = "/"
+	manager.Domain = config.GetEnv("HTTP_HOST")
+	manager.Secure, _ = strconv.ParseBool(config.GetEnv("SESSION_SECURE"))
+	manager.HttpOnly = true
+}
+
+func NewSession(c *gin.Context, cKey string, v Store) {
+	// Generate New Key
+	token := utils.GenRandToken()
+
+	vj, _ := json.Marshal(v)
+
+	if err := r.Set(c, token, vj, 0).Err(); err != nil {
+		log.Panicf("Fatal Error of Register Session 🚫 \n %v", err)
+	}
+
+	c.SetCookie(cKey, token, manager.MaxAge, manager.Path, manager.Domain, manager.Secure, manager.HttpOnly)
+}
+
+func GetSession(c *gin.Context, cKey string) (*Store, error) {
+	token, _ := c.Cookie(cKey)
+	vJSON, err := r.Get(c, token).Bytes()
+	if err == goRedis.Nil {
+		fmt.Println("No Registered Token ⚠")
+		return nil, fmt.Errorf("no registered token %v", err)
+	}
+	if err != nil {
+		fmt.Println("Error has occured in getting Session 🚫")
+		return nil, fmt.Errorf("error has occured in getting session %v", err)
+	}
+
+	// Convert value to store struct
+	v := new(Store)
+	if err := json.Unmarshal(vJSON, v); err != nil {
+		fmt.Println("Error has occured in Unmarshal JSON 🚫")
+		return nil, fmt.Errorf("error has occured in unmarshal json %v", err)
+	}
+	return v, nil
+}
