2ccbb457293b6c06ff995792c8478bc1469fbbfa
Sakyuba Noe <398noe@gmail.com>
2023-03-09 10:09:08 +0900

    Connect redis and make new session manager

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/haisin-official/haisin/config"
 	"github.com/haisin-official/haisin/database"
+	"github.com/haisin-official/haisin/database/redis"
 	"github.com/haisin-official/haisin/routes"
 	_ "github.com/lib/pq"
 )
@@ -17,7 +18,19 @@ func main() {
 	database.InitClient(dbType, dbDSN)
 	defer database.CloseClient()
 
+	// Connect to redis
+	redisUrl := config.GetEnv("REDIS_URL")
+	redis.InitClient(redisUrl)
+	defer redis.CloseClient()
+
+	// Start Gin Framework
 	r := gin.Default()
+
+	// Start New Session Manager with redis
+	redisClient := redis.GetClient()
+	config.NewSessions(redisClient)
+
+	// Add routes for Gin Framework
 	routes.Router(r)
 
 	port := config.GetEnv("HTTP_PORT")
