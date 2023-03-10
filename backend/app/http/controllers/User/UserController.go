4094e40f0df0a7cd0ab68c3bf57cb6bc57ad4341
Sakyuba Noe <398noe@gmail.com>
2023-03-10 12:10:10 +0900

    ユーザーレスポンスを返却するように変更

--- backend/app/http/controllers/User/UserController.go
+++ backend/app/http/controllers/User/UserController.go
@@ -2,8 +2,12 @@ package controllers
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	requests "github.com/haisin-official/haisin/app/http/requests/User"
+	usecases "github.com/haisin-official/haisin/app/usecases/User"
 	"github.com/haisin-official/haisin/config"
 	"github.com/haisin-official/haisin/config/session"
 )
@@ -13,10 +17,45 @@ type UserController struct{}
 func (UserController) GetUserMe(c *gin.Context) {
 
 	ckey := config.GetEnv("SESSION_KEY")
-	data, err := session.GetSession(c, ckey)
+	data, httpCode, err := session.GetSession(c, ckey)
 	if err != nil {
 		fmt.Println(err)
+		c.AbortWithStatus(httpCode)
+		c.JSON(httpCode, gin.H{"error": http.StatusText(httpCode)})
+		return
 	}
+	// セッションから取得したユーザーIDをUUIDにアサーションする
+	u, err := uuid.Parse(data.UserId)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": http.StatusText(http.StatusInternalServerError)})
+		return
+	}
+	// リクエスト用のデータを構築
+	req := requests.UserGetRequest{
+		UserID: u,
+	}
+
+	// dataからUserIDを取得
 	fmt.Println(data.SessionId, data.UserId)
-	// c.JSON(http.StatusOK, userId)
+
+	result, code, err := usecases.UserUseCases{}.UserGetAction(req)
+
+	if err != nil {
+		c.AbortWithStatus(code)
+		c.JSON(code, gin.H{"error": http.StatusText(code)})
+		return
+	}
+
+	c.JSON(code, result)
+}
+
+func (UserController) Logout(c *gin.Context) {
+	ckey := config.GetEnv("SESSION_KEY")
+	if err := session.DeleteSession(c, ckey); err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": http.StatusText(http.StatusInternalServerError)})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{})
 }
