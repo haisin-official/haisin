fc9c429202ec55780cd2f7d0d803bde1c1af783c
Sakyuba Noe <398noe@gmail.com>
2023-03-10 11:16:11 +0900

    Delete conect to redis section

--- backend/main.go
+++ backend/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/haisin-official/haisin/config"
 	"github.com/haisin-official/haisin/database"
-	"github.com/haisin-official/haisin/database/redis"
 	"github.com/haisin-official/haisin/routes"
 	_ "github.com/lib/pq"
 )
@@ -18,17 +17,8 @@ func main() {
 	database.InitClient(dbType, dbDSN)
 	defer database.CloseClient()
 
-	// Connect to redis
-	redisUrl := config.GetEnv("REDIS_URL")
-	redis.InitClient(redisUrl)
-	defer redis.CloseClient()
-
 	// Start Gin Framework
 	r := gin.Default()
-
-	// Start New Session Manager with redis
-	redisClient := redis.GetClient()
-	config.NewSessions(redisClient)
 
 	// Add routes for Gin Framework
 	routes.Router(r)
