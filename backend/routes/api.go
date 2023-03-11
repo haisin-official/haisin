6022df2e7731c7ff99d4d652080c4ed23c5e2e8d
Sakyuba Noe <398noe@gmail.com>
2023-03-11 21:41:44 +0900

    Add authenticate middleware for check session

--- backend/routes/api.go
+++ backend/routes/api.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	OAuthController "github.com/haisin-official/haisin/app/http/controllers/OAuth"
 	UserController "github.com/haisin-official/haisin/app/http/controllers/User"
+	"github.com/haisin-official/haisin/app/http/middleware"
 )
 
 func Router(router *gin.Engine) {
@@ -17,8 +18,10 @@ func Router(router *gin.Engine) {
 	}
 
 	user := v1.Group("/user")
+	user.Use(middleware.Authenticate())
 	{
 		controller := UserController.UserController{}
 		user.GET("/me", controller.GetUserMe)
+		user.POST("/logout", controller.Logout)
 	}
 }
