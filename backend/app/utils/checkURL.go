50711341902c0a79f22b4cb8452a486ca5fcf190
Sakyuba Noe <398noe@gmail.com>
2023-03-14 19:16:07 +0900

    Add check of scheme

--- backend/app/utils/checkURL.go
+++ backend/app/utils/checkURL.go
@@ -10,23 +10,27 @@ var serviceList = map[string]string{
 	"youtube": "www.youtube.com",
 }
 
-func CheckURL(serviceName string, serviceUrl string) (bool, error) {
+func CheckURL(serviceName string, serviceUrl string) error {
 	// string形式のURLをnet/url形式に変換
 	u, err := url.Parse(serviceUrl)
 	if err != nil {
-		return false, err
+		return err
+	}
+	// URLのスキームを抽出
+	if s := u.Scheme; s != "https" {
+		return fmt.Errorf("scheme is not https")
 	}
 	// URLからHostを抽出
 	host := u.Host
 	// hostがserviceListのものと一致するか確認
 	if s, ok := serviceList[serviceName]; ok && s == host {
 		// 一致した場合
-		return true, nil
+		return nil
 	} else if s != host {
 		// hostが一致しなかった場合
-		return false, fmt.Errorf("host name is unmatched with %s", serviceUrl)
+		return fmt.Errorf("host name is unmatched with %s", serviceUrl)
 	}
 
 	// Serviceが見つからなかった場合
-	return false, fmt.Errorf("not found service with %s", serviceName)
+	return fmt.Errorf("not found service with %s", serviceName)
 }
