6137d31873b7c940f6993e9691ae0e0108f746ca
Sakyuba Noe <398noe@gmail.com>
2023-03-15 03:35:11 +0900

    Add Get Controller

--- backend/app/controllers/Service/ServiceController.go
+++ backend/app/controllers/Service/ServiceController.go
@@ -14,6 +14,38 @@ import (
 
 type ServiceController struct{}
 
+func (ServiceController) ServiceGet(c *gin.Context) {
+	cKey := config.GetEnv("SESSION_KEY")
+	data, httpCode, err := session.GetSession(c, cKey)
+	if err != nil {
+		fmt.Println(err)
+		c.AbortWithStatus(httpCode)
+		c.JSON(httpCode, gin.H{"error": http.StatusText(httpCode)})
+		return
+	}
+
+	u, err := uuid.Parse(data.UserId)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": http.StatusText(http.StatusInternalServerError)})
+		return
+	}
+
+	req := requests.ServiceGetRequest{
+		UserID: u,
+	}
+
+	result, code, err := usecases.ServiceUseCase{}.ServiceGetAction(req)
+
+	if err != nil {
+		c.AbortWithStatus(code)
+		c.JSON(code, gin.H{"error": http.StatusText(code)})
+		return
+	}
+
+	c.JSON(code, result)
+}
+
 func (ServiceController) ServicePost(c *gin.Context) {
 	cKey := config.GetEnv("SESSION_KEY")
 	data, httpCode, err := session.GetSession(c, cKey)
