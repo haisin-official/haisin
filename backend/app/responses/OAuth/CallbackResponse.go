46e6f0728f23027a991b1b73c1aeff95c825443f
Sakyuba Noe <398noe@gmail.com>
2023-03-14 17:32:28 +0900

    Add binding type on User and Callback

--- backend/app/responses/OAuth/CallbackResponse.go
+++ backend/app/responses/OAuth/CallbackResponse.go
@@ -8,7 +8,7 @@ type CallbackPostResponse struct {
 
 type CallbackPostUser struct {
 	Uuid  uuid.UUID `json:"uuid" binding:"required"`
-	Email string    `json:"email" binding:"required"`
-	Slug  string    `json:"slug" binding:"required"`
+	Email string    `json:"email" binding:"required, email"`
+	Slug  string    `json:"slug" binding:"required, alphanumunicode, min=4, max=30"`
 	Ga    *string   `json:"ga"`
 }
