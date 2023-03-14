e70e6cfae3303222d0c8922771ac697c803df093
Sakyuba Noe <398noe@gmail.com>
2023-03-15 04:18:28 +0900

    getService, register, update関数を分離

--- backend/app/usecases/Service/ServicePostAction.go
+++ backend/app/usecases/Service/ServicePostAction.go
@@ -18,27 +18,48 @@ import (
 
 func (ServiceUseCase) ServicePostAction(req requests.ServicePostRequest) (responses.ServicePostResponse, int, error) {
 	userId := req.UserID
-	serviceName := req.Service
-	serviceUrl := req.Url
+	name := req.Service
+	redirect := req.Url
+
+	// ServiceNameが存在するか確認
+	if err := service.ServiceValidator(service.Service(name)); err != nil {
+		return responses.ServicePostResponse{}, http.StatusBadRequest, err
+	}
 
 	// URLが正しい形式かどうか確認
-	if err := utils.CheckURL(serviceName, serviceUrl); err != nil {
+	if err := utils.CheckURL(name, redirect); err != nil {
 		return responses.ServicePostResponse{}, http.StatusBadRequest, err
 	}
 	// URLをパース
-	u, err := url.Parse(serviceUrl)
+	u, err := url.Parse(redirect)
 	if err != nil {
 		return responses.ServicePostResponse{}, http.StatusInternalServerError, err
 	}
 
 	// クエリパラメータを削除した登録するURLを生成
-	newUrl := &url.URL{
+	nu := &url.URL{
 		Scheme: "https",
 		Host:   u.Host,
 		Path:   u.Path,
 	}
 
-	service, httpCode, err := register(userId, serviceName, newUrl.String())
+	// userId & name ですでに登録されているデータが存在するか確認する
+	service, _, err := getService(userId, name)
+	// 存在しない場合
+	if service == nil {
+		// insertを行う
+		httpCode, err := register(userId, name, *nu)
+		if err != nil {
+			return responses.ServicePostResponse{}, httpCode, err
+		}
+	} else {
+		// 存在する場合
+		// updateを行う
+
+	}
+
+	// 最新の情報を読み取る
+	service, httpCode, err := getService(userId, name)
 	if err != nil {
 		return responses.ServicePostResponse{}, httpCode, err
 	}
@@ -50,68 +71,98 @@ func (ServiceUseCase) ServicePostAction(req requests.ServicePostRequest) (respon
 	return res, http.StatusOK, nil
 }
 
-func register(userId uuid.UUID, serviceName string, serviceUrl string) (*ent.Service, int, error) {
+func getService(userId uuid.UUID, name string) (*ent.Service, int, error) {
 	client := database.GetClient()
 	ctx := context.Background()
 
-	// 存在するやつにする
-	s := service.Service(serviceName)
-
-	if err := service.ServiceValidator(s); err != nil {
-		// サービスが存在しないのでエラーを返す
-		fmt.Println("not found this service: ", serviceName)
-		return nil, http.StatusUnprocessableEntity, err
+	service, err := client.Service.
+		Query().
+		Select(service.FieldID, service.FieldService, service.FieldURL, service.UserIDColumn).
+		Where(
+			service.And(
+				service.ServiceEQ(service.Service(name)),
+				service.HasUserIDWith(user.IDEQ(userId)),
+			),
+		).Only(ctx)
+	if ent.IsNotFound(err) {
+		return nil, http.StatusNotFound, err
 	}
+
+	if err != nil {
+		return nil, http.StatusInternalServerError, err
+	}
+
+	return service, http.StatusOK, nil
+}
+
+func register(userId uuid.UUID, name string, newURL url.URL) (int, error) {
+	client := database.GetClient()
+	ctx := context.Background()
+
+	if err := service.ServiceValidator(service.Service(name)); err != nil {
+		// サービスが存在しないのでエラーを返す
+		fmt.Println("not found this service: ", name)
+		return http.StatusUnprocessableEntity, err
+	}
+
 	// URLのIDを新しく生成
 	u, err := utils.GenUUID()
 	if err != nil {
-		return nil, http.StatusInternalServerError, err
+		return http.StatusInternalServerError, err
 	}
 
 	// upsertはconflictに指定するカラムがuniqueの必要があるため使用できない。oauthの時と同様、丁寧にinsertすべきかupdateすべきか分岐させる必要がある
 	if err := client.Service.
 		Create().
 		SetID(u).
-		SetService(s).
-		SetURL(serviceUrl).
+		SetService(service.Service(name)).
+		SetURL(newURL.String()).
 		SetUserIDID(userId).
-		OnConflictColumns(
-			service.FieldService,
-			service.UserIDColumn,
-		).
-		UpdateNewValues().
-		Exec(ctx); ent.IsNotFound(err) {
-		return nil, http.StatusNotFound, err
-	} else if ent.IsValidationError(err) {
-		return nil, http.StatusUnprocessableEntity, err
+		Exec(ctx); ent.IsValidationError(err) {
+		return http.StatusUnprocessableEntity, err
 	} else if ent.IsConstraintError(err) {
-		return nil, http.StatusConflict, err
+		return http.StatusConflict, err
 	}
 	if err != nil {
-		return nil, http.StatusInternalServerError, fmt.Errorf("something error has occured \n%v", err)
+		return http.StatusInternalServerError, fmt.Errorf("something error has occured \n%v", err)
 	}
 
-	// 新しく取得したサービスとURLを取得
-	service, err := client.Service.
-		Query().
-		Select(service.FieldService, service.FieldURL).
-		Where(
-			service.And(
-				service.ServiceEQ(s),
-				service.HasUserIDWith(user.IDEQ(userId)),
-			),
-		).Only(ctx)
+	return http.StatusOK, nil
 
-	if ent.IsNotFound(err) {
-		return nil, http.StatusNotFound, err
-	} else if ent.IsValidationError(err) {
-		return nil, http.StatusUnprocessableEntity, err
+}
+
+func update(userId uuid.UUID, name string, newURL url.URL) (int, error) {
+	client := database.GetClient()
+	ctx := context.Background()
+
+	if err := service.ServiceValidator(service.Service(name)); err != nil {
+		// サービスが存在しないのでエラーを返す
+		fmt.Println("not found this service: ", name)
+		return http.StatusUnprocessableEntity, err
+	}
+
+	// URLのIDを新しく生成
+	u, err := utils.GenUUID()
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+
+	// upsertはconflictに指定するカラムがuniqueの必要があるため使用できない。oauthの時と同様、丁寧にinsertすべきかupdateすべきか分岐させる必要がある
+	if err := client.Service.
+		Create().
+		SetID(u).
+		SetService(service.Service(name)).
+		SetURL(newURL.String()).
+		SetUserIDID(userId).
+		Exec(ctx); ent.IsValidationError(err) {
+		return http.StatusUnprocessableEntity, err
 	} else if ent.IsConstraintError(err) {
-		return nil, http.StatusConflict, err
+		return http.StatusConflict, err
 	}
 	if err != nil {
-		return nil, http.StatusInternalServerError, fmt.Errorf("something error has occured \n%v", err)
+		return http.StatusInternalServerError, fmt.Errorf("something error has occured \n%v", err)
 	}
 
-	return service, http.StatusOK, nil
+	return http.StatusOK, nil
+
 }
