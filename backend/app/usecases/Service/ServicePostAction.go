09f5ad9a0c408afb0488f8dbbdd008155049bfda
Sakyuba Noe <398noe@gmail.com>
2023-03-15 02:30:04 +0900

    Make register function

--- backend/app/usecases/Service/ServicePostAction.go
+++ backend/app/usecases/Service/ServicePostAction.go
@@ -1,6 +1,8 @@
 package usecases
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"net/url"
 
@@ -8,7 +10,9 @@ import (
 	requests "github.com/haisin-official/haisin/app/requests/Service"
 	responses "github.com/haisin-official/haisin/app/responses/Service"
 	"github.com/haisin-official/haisin/app/utils"
+	"github.com/haisin-official/haisin/database"
 	"github.com/haisin-official/haisin/ent"
+	"github.com/haisin-official/haisin/ent/service"
 )
 
 func (ServiceUseCase) ServicePostAction(req requests.ServicePostRequest) (responses.ServicePostResponse, int, error) {
@@ -33,18 +37,69 @@ func (ServiceUseCase) ServicePostAction(req requests.ServicePostRequest) (respon
 		Path:   u.Path,
 	}
 
-	resultUrl, httpCode, err := register(userId, serviceName, newUrl.String())
+	service, httpCode, err := register(userId, serviceName, newUrl.String())
 	if err != nil {
 		return responses.ServicePostResponse{}, httpCode, err
 	}
 
 	res := responses.ServicePostResponse{
-		Service: resultUrl.String(),
-		Url:     resultUrl.URL,
+		Service: service.Service.String(),
+		Url:     service.URL,
 	}
 	return res, http.StatusOK, nil
 }
 
-func register(userId uuid.UUID, serviceName string, serviceUrl string) (*ent.Url, int, error) {
-	return nil, http.StatusOK, nil
+func register(userId uuid.UUID, serviceName string, serviceUrl string) (*ent.Service, int, error) {
+	client := database.GetClient()
+	ctx := context.Background()
+
+	// 存在するやつにする
+	s := service.Service(serviceName)
+
+	if err := service.ServiceValidator(s); err != nil {
+		// サービスが存在しないのでエラーを返す
+		return nil, http.StatusUnprocessableEntity, err
+	}
+
+	err := client.Service.
+		Create().
+		SetID(userId).
+		SetService(s).
+		OnConflictColumns(service.FieldService).
+		UpdateNewValues().
+		SetURL(serviceUrl).
+		Exec(ctx)
+
+	if ent.IsNotFound(err) {
+		return nil, http.StatusNotFound, err
+	} else if ent.IsValidationError(err) {
+		return nil, http.StatusUnprocessableEntity, err
+	} else if ent.IsConstraintError(err) {
+		return nil, http.StatusConflict, err
+	}
+	if err != nil {
+		return nil, http.StatusInternalServerError, fmt.Errorf("something error has occured \n%v", err)
+	}
+
+	// 新しく取得したサービスとURLを取得
+	service, err := client.Service.
+		Query().
+		Unique(true).
+		Select(service.FieldService, service.FieldURL).
+		Where(service.IDEQ(userId)).
+		Where(service.ServiceIn(s)).
+		Only(ctx)
+
+	if ent.IsNotFound(err) {
+		return nil, http.StatusNotFound, err
+	} else if ent.IsValidationError(err) {
+		return nil, http.StatusUnprocessableEntity, err
+	} else if ent.IsConstraintError(err) {
+		return nil, http.StatusConflict, err
+	}
+	if err != nil {
+		return nil, http.StatusInternalServerError, fmt.Errorf("something error has occured \n%v", err)
+	}
+
+	return service, http.StatusOK, nil
 }
