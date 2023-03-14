5884e52da69fe42521087de956cd9d37fe232375
Sakyuba Noe <398noe@gmail.com>
2023-03-15 04:55:16 +0900

    Add delete controller on service controller

--- backend/app/controllers/Service/ServiceController.go
+++ backend/app/controllers/Service/ServiceController.go
@@ -10,6 +10,7 @@ import (
 	usecases "github.com/haisin-official/haisin/app/usecases/Service"
 	"github.com/haisin-official/haisin/config"
 	"github.com/haisin-official/haisin/config/session"
+	"github.com/haisin-official/haisin/ent/service"
 )
 
 type ServiceController struct{}
@@ -77,6 +78,47 @@ func (ServiceController) ServicePost(c *gin.Context) {
 	}
 
 	result, code, err := usecases.ServiceUseCase{}.ServicePostAction(req)
+
+	if err != nil {
+		c.AbortWithStatus(code)
+		c.JSON(code, gin.H{"error": http.StatusText(code)})
+		return
+	}
+
+	c.JSON(code, result)
+}
+
+func (ServiceController) ServiceDelete(c *gin.Context) {
+	cKey := config.GetEnv("SESSION_KEY")
+	data, httpCode, err := session.GetSession(c, cKey)
+	if err != nil {
+		fmt.Println(err)
+		c.AbortWithStatus(httpCode)
+		c.JSON(httpCode, gin.H{"error": http.StatusText(httpCode)})
+		return
+	}
+
+	u, err := uuid.Parse(data.UserId)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": http.StatusText(http.StatusInternalServerError)})
+		return
+	}
+	// パスからサービス名を取得する
+	name := c.Param("name")
+	// サービス名が含まれていなければBad Requestを返す
+	// entのserviceからValidatorを引き連れているので、ginのservice Validatorを使用したい
+	if err := service.ServiceValidator(service.Service(name)); err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		c.JSON(http.StatusBadRequest, gin.H{"error": http.StatusText(http.StatusBadRequest)})
+		return
+	}
+
+	req := requests.ServiceDeleteRequest{
+		UserID:  u,
+		Service: name,
+	}
+	result, code, err := usecases.ServiceUseCase{}.ServiceDeleteAction(req)
 
 	if err != nil {
 		c.AbortWithStatus(code)
