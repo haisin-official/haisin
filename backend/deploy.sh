75eb94ce04af8946e40b245461cba54956bcebbe
Sakyuba Noe <398noe@gmail.com>
2023-03-19 21:19:43 +0900

    Add other command for deploy to kubectl

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -9,4 +9,15 @@ kubectl apply -f database-secret.yaml
 kubectl apply -f redis-secret.yaml
 kubectl apply -f app-secret.yaml
 
-kubectl apply -f app-
+kubectl apply -f database-pv.yaml
+kubectl apply -f database-pvc.yaml
+kubectl apply -f database-deployment.yaml
+kubectl apply -f database-service.yaml
+
+kubectl apply -f redis-pv.yaml
+kubectl apply -f redis-pvc.yaml
+kubectl apply -f redis-deployment.yaml
+kubectl apply -f redis-service.yaml
+
+kubectl apply -f app-deployment.yaml
+kubectl apply -f app-service.yaml
