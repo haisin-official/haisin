f70715e31a49d26e4724ca3d5ce63b2c4e818d80
Sakyuba Noe <398noe@gmail.com>
2023-03-19 12:12:44 +0900

    Add init function for switch development environments

--- backend/config/dotenv.go
+++ backend/config/dotenv.go
@@ -7,6 +7,16 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func init() {
+	if v := os.Getenv("GO_ENV"); v != "production" {
+		err := godotenv.Load(".env")
+
+		if err != nil {
+			panic(".env is not found")
+		}
+	}
+}
+
 func GetEnv(name string) string {
 	err := godotenv.Load(".env")
 
