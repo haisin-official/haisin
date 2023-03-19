1ad2caac25cde57955383dc5ffc3b2960f09365f
Sakyuba Noe <398noe@gmail.com>
2023-03-19 12:14:33 +0900

    Add go env for build command

--- backend/build.sh
+++ backend/build.sh
@@ -1,1 +1,1 @@
-go build -o main main.go
+GO_ENV=production go build -o main main.go
--- backend/config/dotenv.go
+++ backend/config/dotenv.go
@@ -8,6 +8,7 @@ import (
 )
 
 func init() {
+	// production環境以外の場合, .envファイルから環境変数を読み込む
 	if v := os.Getenv("GO_ENV"); v != "production" {
 		err := godotenv.Load(".env")
 
