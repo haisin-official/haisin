32261e678f93889fb000e57fb833790adfa164bf
Sakyuba Noe <398noe@gmail.com>
2023-03-20 00:33:11 +0900

    Add migration yaml for deploy command

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -7,18 +7,26 @@ docker push backend
 # apply kubernetes
 kubectl apply -f database-secret.yaml
 kubectl apply -f redis-secret.yaml
+kubectl apply -f migration-secret.yaml
 kubectl apply -f app-secret.yaml
 
+# Execute database
 kubectl apply -f database-pv.yaml
 kubectl apply -f database-pvc.yaml
 kubectl apply -f database-cm.yaml
 kubectl apply -f database-deployment.yaml
 kubectl apply -f database-service.yaml
 
+# Execute redis
 kubectl apply -f redis-pv.yaml
 kubectl apply -f redis-pvc.yaml
 kubectl apply -f redis-deployment.yaml
 kubectl apply -f redis-service.yaml
 
+# Execute migrations
+kubectl apply -f migration-deployment.yaml
+kubectl apply -f migration-service.yaml
+
+# Execute golang apps
 kubectl apply -f app-deployment.yaml
 kubectl apply -f app-service.yaml
