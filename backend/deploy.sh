de2f16cc9b37b75b9127eeb08189044f0244e282
Sakyuba Noe <398noe@gmail.com>
2023-03-19 21:03:58 +0900

    Fix typo for RFC1123

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 # build backend image
-docker build -t haisin_official/backend:latest -f docker/production/app/Dockerfile .
+docker build -t haisin-official/backend:latest -f docker/production/app/Dockerfile .
 docker push backend
+
+# apply kubernetes
+kubectl apply -f database-secret.yaml
+kubectl apply -f redis-secret.yaml
+kubectl apply -f app-secret.yaml
+
+kubectl apply -f app-
